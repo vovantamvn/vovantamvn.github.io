3dee54f2c6281b93176be7135eeb96cfacc1fde8
tamvv <vovantam.dev@gmail.com>
2021-04-02 15:52:15 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,53 @@
 module.exports = {
-  siteMetadata: {
-    title: `Tâm's Blog`,
-    description: `Đây là blog chia sẻ kiến thức`,
-    author: `vovantamvn`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        siteUrl: `https://vovantamvn.github.io`,
+        title: `Tâm's Blog`,
+        description: `Đây là blog chia sẻ kiến thức`,
+        author: `Võ Văn Tâm`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `markdown-pages`,
-          path: `${__dirname}/src/markdown-pages`,
+    plugins: [
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
         },
-    },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/markdown-pages`,
-          name: `markdown-pages`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
         },
-    },
-    `gatsby-transformer-remark`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        `gatsby-plugin-gatsby-cloud`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `${__dirname}/src/markdown-pages`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/markdown-pages`,
+                name: `markdown-pages`,
+            },
+        },
+        `gatsby-transformer-remark`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
