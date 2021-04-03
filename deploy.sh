c2e15aa62960192bd49f21ee51aa3aa1f48a2dc5
tamvv <vovantam.dev@gmail.com>
2021-04-03 17:58:51 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 rm -rf .git/
 cd public
+
+git config --global user.email "vovantam.dev@gmail.com"
+git config --global user.name "tamvv"
+
 git init
+git remote add origin git@github.com:vovantamvn/vovantamvn.github.io.git
+
 git checkout -b gh-pages
-remote add origin git@github.com:vovantamvn/vovantamvn.github.io.git  
 git add .
 git commit -m "Deploy by circleci"
 git push origin gh-pages -f
