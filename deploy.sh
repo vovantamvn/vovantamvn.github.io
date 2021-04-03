5e1e428ce19e9ad6eb4a1d0f6245a7bee34b5aff
tamvv <vovantam.dev@gmail.com>
2021-04-03 17:54:36 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-echo "Build"
-ls
-cd build
-ls
+rm -rf .git/
+cd public
+git init
+git checkout -b gh-pages
+remote add origin git@github.com:vovantamvn/vovantamvn.github.io.git  
+git add .
+git commit -m "Deploy by circleci"
+git push origin gh-pages -f
+echo "Deploy success!"
