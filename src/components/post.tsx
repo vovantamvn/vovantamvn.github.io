716de529799e39d5e6c8fdb4f13fcfb27f0d5987
Võ Văn Tâm <vovantam.dev@gmail.com>
2021-05-07 16:23:04 +0700

    Features/home (#5)
    * Init home page
    
    * show all post
    
    * format code

---
+++ src/components/post.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link } from "gatsby"
+import kebabCase from "lodash/kebabCase"
+import "./post.css"
+
+interface Props {
+  node: {
+    frontmatter: {
+      date: string
+      title: string
+      slug: string
+      tags: Array<string>
+    }
+    timeToRead: number
+  }
+}
+
+export default function Post({ node }: Props) {
+  const { frontmatter } = node
+  const { title, slug, date, tags } = frontmatter
+
+  return (
+    <div className="post-container card">
+      <Link className="post-title link non-text-decoration" to={slug}>
+        {title}
+      </Link>
+      <div>
+        {tags.map(tag => (
+          <span className="tag">
+            <Link
+              className="non-text-decoration"
+              to={`/tags/${kebabCase(tag)}`}
+            >
+              {tag}
+            </Link>
+          </span>
+        ))}
+      </div>
+      <p>Date: {date}</p>
+    </div>
+  )
+}
